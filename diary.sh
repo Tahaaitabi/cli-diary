f32a8b2d7fef99718d661a19c6462e266424568f
archTaha <aitabitaha@gmail.com>
2024-07-27 13:49:10 +0200

    Fixed some issues with functions for saving the file & updated readme

--- diary.sh
+++ diary.sh
@@ -3,14 +3,17 @@
 name=$(whoami)
 date="$(date +"%d/%m/%y")"
 time="$(date +"%H:%M %p")"
+path=~/cli-diary  
+prompt=$'\n> '
+
 
 #Greeting:
 function greeting {
   if [ $(date +"%p") = "AM" ]; then
     greeting="Good Morning"
-  elif [ $(date +"%p") = "PM" ] && [ $(date +"%H") -ge 5 ]; then
+  elif [ $(date +"%p") = "PM" ] && [ $(date +"%H") -le 16 ]; then
     greeting="Good Afternoon"
-  elif [ $(date +"%p") = "PM" ] && [ $(date +"%H") -le 5 ]; then
+  elif [ $(date +"%p") = "PM" ] && [ $(date +"%H") -ge 17 ]; then
     greeting="Good Evening"
   fi
   clear
@@ -19,70 +22,97 @@ function greeting {
 }
 
 # [y/n]:
-function yn_choice {
-  printf "\nType [y] for [YES] | [n] for [NO] "
+function yn {
+  printf "\nType [y] for [YES] | [n] for [NO]$prompt"
   read choice
   if [ $choice != 'y' -a $choice != 'n' ]; then 
     while [ $choice != 'y' -a $choice != 'n' ]; do  
-      printf "\nInvalid choice!\n \nType a lowercase [y]/[n] for [YES]/[NO] and then press [ENTER] "
+      printf "\nInvalid choice!\n \nType a lowercase [y]/[n] for [YES]/[NO] and then press [ENTER]$prompt"
       read choice
     done
+  fi
+}
+
+# Path checker;
+function check_path {
+  if [ ! -d $path ]; then
+    echo "...creating the diary's directory in '~'"
+    $(mkdir $path)
+    sleep 1
+  fi
+}
+
+# Save file:
+function save_file {
+  if [ -d $path ]; then
+    printf "**ID:** $id\n**Added on:** $entry_date at $entry_time\n**Title:** # $title\n**Content:** $content\n" > $path/$file_name.md
+    echo "Saved as $file_name.md on $date at $time, in $path"
+    echo ""
+    back_to_main
+  else 
+    check_path
+    save_file
   fi
 }
 
 # Preview entry data:
 function preview { 
   clear
-  echo "**PREVIEW**:"
-  echo ""
   echo "Added on:   [ $entry_date ]"
   echo "Title:      [ $title ]"
   echo "Content:    [ $content ]"
+  echo "Save as:    [ $file_name.md ]"
   echo ""
 }
 
 # Back to main menu:
 function back_to_main {
-  echo "Press [Enter] to return to the main menu: "  
-  read 
+  read -p "Press [Enter] to return to the main menu:$prompt"
   main
 }
 
 # Create new record:
 function new_record { 
   clear
-  read -p "Enter a title: " title
-  read -p "Enter the content: " content
+  read -p "Enter a title:$prompt" title
+  read -p "Enter the content:$prompt" content
   clear
-  #printf "[Preview]:\n \n[Title]: $title\n[Content]: $content"
   preview
-  printf "Use today's date ?\n"  
-  yn_choice
+  printf "Use today's date ?"  
+  yn 
   if [ $choice = 'y' ]; then
     entry_date=$date
+    entry_time=$time
     clear
     printf "Date added sucessfully!\n"
-    #printf "[Preview]:\n \n[Title]: $title\n[Content]: $content\n[Added on]: $entry_date\n"
     clear
     preview
-    back_to_main
   elif [ $choice = 'n' ]; then
     clear
-    printf "Please enter a date [DD/MM/YY]: "
+    printf "Please enter a date [DD/MM/YY]:$prompt"
     read entry_date
-    #printf "\n[Preview]:\n \n[Title]: $title\n[Content]: $content\n[Added on]: $entry_date\n"
+    entry_time=$time
     clear
     preview
-    back_to_main
   fi
-  # create a folder in the home directory and save file
+
+  # Name the file
+  echo -n "Please name your file:$prompt" 
+  read file_name
+
+  # Set the id:
+  id="md-$file_name-$entry_date"
+
+  # Save file:
+  save_file
 }
 
-
+# Fetch record:
 function get_record {
   clear
 }
 
+# Edit entry
 function edit_record {
   printf "\nEditing records..."
   sleep 1
@@ -95,7 +125,7 @@ function main {
   while true; do
     printf "What would you like to do ?"
     echo ""
-    printf " 1. Create a new record\n 2. Retrieve a record\n 3. Edit a record\n 4. Exit diary\n"
+    printf " 1. Create a new record\n 2. Retrieve a record\n 3. Edit a record\n 4. Exit diary$prompt"
     read menu_choice 
     case $menu_choice in
       1 )
--- readme.md
+++ readme.md
@@ -1,14 +1,10 @@
 # Diary program for the cli written in Bash
-
-The program should be executed by running "diary" in the cli.
-Make sure to add an alias for it in the bashrc.
-
-### Program concept:
 ---
-The program should be able to:
+## Concept:
 ---
+The program should be able to do the following:
 
-1. Welcome screen -> `welcome()`
+1. Greet the user and show a menu:
     - a. `greeting()` -> [done]
     - b. `main()` -> Function with a case with 4 options; create, retireve, edit and exit. -> [done]
     
@@ -18,11 +14,11 @@ The function should create the following variables:
     - a. `$title` [done]
     - b. `$content` [done]
     - c. `$date` [done]
-    - d. `$time` 
-    - e. `$id`
-    - f. `double_check()` -> Check that there isn't a duplicate record with the same id or title with the same date. If there is, then return an error message and instructions for a valid entry.
+    - d. `$time` [done]
+    - e. `$id` [done]
+    - f. `check_path()` -> Check if a directory called "cli-diary"  exists in the home directory and if not create it. [done]
 ---
-3. Retrieve a record -> `get_record()`
+3. Retrieve a record -> `get_record()` [**in-progress**]
 This function should be able to search the file in which the entries are saved, and fetch a list of entry titles, date, time and the first 2 lines of the entry.
     - a. `$title`
     - b. `$preview`
@@ -32,3 +28,6 @@ This function should be able to search the file in which the entries are saved, 
     - f. Code to read from the database.[todo]
 ---
 4. Edit a record -> `edit_record()`
+This function should be able to edit an entry as well as create some variables to save the history of the changes made to an entry, a sort of mini git inspired system to keep track of the history.
+
+>Note: You're more than welcome to fork this and create your  own version of it if you want. If you have any advice and pointers on what I could do to make it better please get in touch!
