cd663a7c4fa7bc14a330f3452800fa78bfe18cf4
Taha <aitabitaha@gmail.com>
2024-08-16 14:49:49 +0200

    added better fn's to navigate + more

--- diary.sh
+++ diary.sh
@@ -1,4 +1,6 @@
-# GLOBAL VARIABLES:
+#####################################
+# CONFIGURATION AND GLOBAL VARIABLES:
+#####################################
 name=$(whoami)
 date="$(date +"%d/%m/%y")"
 time="$(date +"%H:%M %p")"
@@ -6,12 +8,11 @@ path=~/cli-diary/
 db_path=~/cli-diary/db/
 prompt=$(printf "\n[>] ")
 id="md-$file_name-$entry_date"
-
-###############
-# START SCREEN:
-###############
-
-#Greeting:
+db_name="$db.db"
+#####################################
+# INITIALIZATION AND SETUP:
+#####################################
+# Greeting:
 function greeting {
   if [ $(date +"%p") = "AM" ]; then
     greeting="Good Morning"
@@ -24,9 +25,7 @@ function greeting {
   printf "$greeting, $name!\n"
   sleep .5
 }
-
-
-# Path checker;
+# Check if cli-diary dir exists:
 function check_path {
   if [ ! -d $path ]; then
     echo "...creating the diary's directory in '~'"
@@ -34,39 +33,19 @@ function check_path {
     sleep 1
   fi
 }
-
-
-#####################
-# PREVIEW FUNCTIONS:
-#####################
-
-# Preview entry data:
-function preview { 
-  clear
-  echo "Added on:   [ $entry_date ]"
-  echo "Title:      [ $title ]"
-  echo "Content:    [ $content ]"
-  echo "Save as:    [ $file_name.md ]"
-  echo ""
-}
-
-#############
-# NAVIGATION:
-#############
-
-# [y/n]:
-function yn {
-  printf "\nType [y] for [YES] | [n] for [NO]$prompt"
-  read choice
-  if [ $choice != 'y' -a $choice != 'n' ]; then 
-    while [ $choice != 'y' -a $choice != 'n' ]; do  
-      printf "\nInvalid choice!\n \nType a lowercase [y]/[n] for [YES]/[NO] and then press [ENTER]$prompt"
-      read choice
-    done
+# Check database directory exists:
+function db_path_check {
+  if [ ! -d $db_path ]; then
+    $(mkdir $db_path)
+    e="then"
+  else
+    e="else"
   fi
 }
-
-# Function for the main menu:
+#####################################
+# NAVIGATION AND MENUS:
+#####################################
+# The main menu:
 function main {
   clear
   while true; do
@@ -94,34 +73,45 @@ function main {
     esac
   done
 }
-
+# DB menu:
+function db_menu {
+  clear
+  echo "1) Search for a different database."
+  echo "2) Show available database's."
+  echo "3) Create a new database."
+  echo "4) Go back to the previous menu."
+  read -p "What would you like to do ? $prompt" opt
+  case $opt in
+    1) db_search
+      ;;
+    2) db_show
+      ;;
+    3) db_create
+      ;;
+    4) back_to-retrieve
+      ;;
+  esac
+}
 # Back to main menu:
 function back_to_main {
-  read -p "Press [Enter] to return to the main menu:$prompt"
+  read -p "Press [Enter] to return to the main menu:$prompt" opt
   main
 }
-
 # Back to Retrieve records:
 function back_to_retrieve {
-  read -p "Press [Enter] to return to the previous menu:$prompt"
+  read -p "Press [Enter] to return to the previous menu:$prompt" opt
   get_record
 }
-
-############
-# DATABASE:
-############
-#if the db directory does not exist, create it.
-function db_path_check {
-  if [ ! -d $db_path ]; then
-    $(mkdir $db_path)
-    e="then"
-  else
-    e="else"
-  fi
+# Back to db_menu
+function back_to_db_menu {
+  read -p "Press [Enter] to return to the previous menu:$prompt" opt
+  db_menu
 }
-
+#####################################
+# DATABASE OPERATIONS:
+#####################################
+# Check the path of the db:
 function db_check {
-  #Checks if a directory is empty and if it is it creates a database, if it is not empty it just returns the names of all '.db' files it finds.
   db_path_check
   empty_dir=$(ls -A $db_path | wc -w )
   search_db=$(find $db_path -name "*.db" | wc -w )
@@ -136,10 +126,8 @@ function db_check {
       sleep .3
       echo "Done!"
     else
-      clear
-      echo "Database/s found: "
-      echo "$(find $db_path -name "*.db" | awk -F "/" '{print "[" $NF "]"}')"
       sleep .3
+      clear
     fi
   else 
     echo "This is an empty directory!"
@@ -151,27 +139,7 @@ function db_check {
   fi
 }
 
-function select_table {
-  PS3="Select a Table to view: "
-  select table in $tables 
-  do
-    if [ -n "$table" ]; then 
-      clear
-      echo "You selected: [$table]"
-      break
-    else 
-      echo "Invalid selection. Please try again."
-      sleep 1
-    fi
-  done
-  selection="$table"
-}
-
-function create_new_table {
-  entries=$(sqlite3 $db_path$db_name "CREATE TABLE entries ( id INTEGER PRIMARY KEY, title TEXT NOT NULL, content TEXT NOT NULL, created DATETIME DEFAULT CURRENT_TIMESTAMP );")
-  $entries
-}
-
+# Get table data
 function dbdata {
   if [ $(sqlite3 $db_path$db_name ".tables" | wc -w ) -ne  "1" ]; then 
     # get the tables in db
@@ -192,7 +160,6 @@ function dbdata {
     table_rows=$(echo $table_data | awk -F "[()]" '{print $2}' | awk -F "," '{print $1; print $2; print $3}' | awk '{print "> "$1}')
     dbdata_e="else"
   fi
-
   # If the database is empty:
   if [ $(sqlite3 $db_path$db_name ".tables" | wc -w ) =  "0" ]; then 
     echo "This database is empty, would you like me to create the required entries ? "
@@ -215,16 +182,28 @@ function dbdata {
         back_to_retrieve
       fi
     else
-      break 
+      back_to_retrieve 
     fi
   fi
 }
+# Show available db's:
+function db_show {
+  echo "Database/s found: "
+  echo "$(find $db_path -name "*.db" | awk -F "/" '{print "[" $NF "]"}')"
+  sleep .3
 
-function get_db {
-  db_check
+}
+# Create a new db:
+function db_create {
   read -p "Enter a db name: " db
-  db_name="$db.db"
-  if [ $(find $db_path$db_name >> /dev/null; echo $? ) = '0' ]; then
+  create_db=$(cd "$db_path" && touch "$db".db )
+  echo "Created '"$db".db', in "$db_path" "; 
+  sleep .8
+}
+# Search for a db:
+function db_search {
+  read -p "Enter a db name: " db
+  if [ $(find "$db_path$db".db >> /dev/null; echo $? ) = '0' ]; then
     echo "Loading "$db" data..."; 
     sleep .5
     clear
@@ -242,28 +221,43 @@ function get_db {
       back_to_retrieve
     fi
   else
-    echo "$db does not exist! Please check the name and try again.";
-    sleep .5
-    clear
+    echo "$db does not exist!";
+    sleep 1
+    db_menu
   fi
 }
-###########
-# WRITING:
-###########
-
-# Save file:
-function save_file {
-  if [ -d $path ]; then
-    printf "**ID**: $id\n**Added on**: $entry_date at $entry_time\n**Title**: # $title\n**Content**: $content\n" >> $path$file_name.md
-    echo "Saved as $file_name.md on $date at $time, in $path"
-    echo ""
-    back_to_main
-  else 
-    check_path
-    save_file
-  fi
+# Retrieve database:
+function get_db {
+  db_check
+  db_search
+}
+# TABLE RELATED:
+#---------------
+# Select Table in a DB:
+function select_table {
+  PS3="Select a Table to view: "
+  select table in $tables 
+  do
+    if [ -n "$table" ]; then 
+      clear
+      echo "You selected: [$table]"
+      break
+    else 
+      echo "Invalid selection. Please try again."
+      sleep 1
+    fi
+  done
+  selection="$table"
 }
 
+function create_new_table {
+  entries=$(sqlite3 $db_path$db.db "CREATE TABLE entries ( id INTEGER PRIMARY KEY, title TEXT NOT NULL, content TEXT NOT NULL, created DATETIME DEFAULT CURRENT_TIMESTAMP );")
+  $entries
+}
+
+#####################################
+# FILE OPERATIONS:
+#####################################
 # Create a new record:
 function new_record { 
   clear
@@ -297,11 +291,18 @@ function new_record {
   id="md-$file_name-$entry_date" # Save file:
   save_file
 }
-
-#############
-# SEARCHING:
-#############
-
+# Save file:
+function save_file {
+  if [ -d $path ]; then
+    printf "**ID**: $id\n**Added on**: $entry_date at $entry_time\n**Title**: # $title\n**Content**: $content\n" >> $path$file_name.md
+    echo "Saved as $file_name.md on $date at $time, in $path"
+    echo ""
+    back_to_main
+  else 
+    check_path
+    save_file
+  fi
+}
 # Search by name 
 function search_name { 
   clear
@@ -322,7 +323,7 @@ function search_name {
         ;;
       2) echo "Opening in vim soon come!..."
         ;;
-      3) main_menu
+      3) main
         ;;
     esac
 
@@ -331,7 +332,6 @@ function search_name {
     search_name
   fi
 }
-
 # Search by word 
 function search_word {
   clear
@@ -349,21 +349,16 @@ function search_word {
     echo "The word you're looking for does not exist."
     echo ""
     echo  "Would you like to search for another term ?" 
-  fi
-  yn
-  if [ $choice = 'y' ]; then
-    search_word
-  elif [ $choice = 'n' ]; then
-    echo "Returning to the main menu..."
-    sleep 0.5
-    main
+    yn
+    if [ $choice = 'y' ]; then
+      search_word
+    elif [ $choice = 'n' ]; then
+      echo "Returning to the main menu..."
+      sleep 0.5
+      main
+    fi
   fi
 }
-
-###########
-# FETCHING:
-###########
-
 # Retrieve records:
 function get_record {
   clear
@@ -372,8 +367,7 @@ function get_record {
   echo "3) Search by name."
   echo "4) Search by database"
   echo "5) Back to main-menu"
-  echo ""
-  read -p "How would you like to search ? (type in the number [1-4] and press ENTER) $prompt" opt
+  read -p "How would you like to search ? (type in the number [1-5] and press ENTER) $prompt" opt
   case $opt in
     1 ) search_word
       ;;
@@ -386,17 +380,37 @@ function get_record {
     5 ) main 
   esac
 }
-
-###########
-# EDITING:
-###########
-
 # Edit record:
 function edit_record {
-  printf "\nEditing records..."
+  printf "\n FUNCTION IN PROGRESS OF CREATION..."
   sleep 1
   clear
 }
-
+#####################################
+# UTILITY FUNCTIONS:
+#####################################
+# Preview of what a file looks like:
+function preview { 
+  clear
+  echo "Added on:   [ $entry_date ]"
+  echo "Title:      [ $title ]"
+  echo "Content:    [ $content ]"
+  echo "Save as:    [ $file_name.md ]"
+  echo ""
+}
+# [y/n] options:
+function yn {
+  printf "\nType [y] for [YES] | [n] for [NO]$prompt"
+  read choice
+  if [ $choice != 'y' -a $choice != 'n' ]; then 
+    while [ $choice != 'y' -a $choice != 'n' ]; do  
+      printf "\nInvalid choice!\n \nType a lowercase [y]/[n] for [YES]/[NO] and then press [ENTER]$prompt"
+      read choice
+    done
+  fi
+}
+#####################################
+# SCRIPT ENTRY POINT:
+#####################################
 greeting
 main
